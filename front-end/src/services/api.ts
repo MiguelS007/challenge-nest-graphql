aa4aebc3220e722a51485984fb43d7538d042a6d
MiguelS007 <miguelsodre007@gmail.com>
2020-11-26 10:38:53 -0300

    update

--- front-end/src/services/api.ts
+++ front-end/src/services/api.ts
@@ -1,6 +1,6 @@
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import ApolloClient from 'apollo-boost';
 
-export default new ApolloClient<InMemoryCache>({
+export default new ApolloClient({
   uri: 'http://localhost:3333/graphql',
   // request: (operation) => {
   //   operation.setContext(({ headers = {} }) => ({
